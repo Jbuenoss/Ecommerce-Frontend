ea983450383ee6934c727fe150cfadf1aa0833b5
Joao Bueno <joaobueno121212@gmail.com>
2024-06-16 20:43:03 -0300

    initial layout for products created

--- Ecommerce/src/routes/Products.jsx
+++ Ecommerce/src/routes/Products.jsx
@@ -1,10 +1,50 @@
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import './style.css';
 
-function Products(){
+import photoProduct from '../assets/Generic product.jpg';
 
-    return(
-        <section>
-            products
-        </section>
+function Products() {
+
+    return (
+        <Container className='d-flex flex-wrap justify-content-center'>
+            <Card className='card-ctm mt-3'>
+                <Card.Img className='card-img-ctm' variant="top" src={photoProduct} />
+                <Card.Body className='card-body-ctm d-flex flex-column' >
+                    <Card.Title>name of Card</Card.Title>
+                    <Card.Text className='mt-auto mb-1'>
+                        price:
+                    </Card.Text>
+                    <Card.Text>
+                        Quantity:
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='card-ctm mt-3 ms-3'>
+                <Card.Img className='card-img-ctm' variant="top" src={photoProduct} />
+                <Card.Body className='card-body-ctm d-flex flex-column' >
+                    <Card.Title>name of Card</Card.Title>
+                    <Card.Text className='mt-auto mb-1'>
+                        price:
+                    </Card.Text>
+                    <Card.Text>
+                        Quantity:
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='card-ctm mt-3 ms-3'>
+                <Card.Img className='card-img-ctm' variant="top" src={photoProduct} />
+                <Card.Body className='card-body-ctm d-flex flex-column' >
+                    <Card.Title>name of Card</Card.Title>
+                    <Card.Text className='mt-auto mb-1'>
+                        price:
+                    </Card.Text>
+                    <Card.Text>
+                        Quantity:
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
 
