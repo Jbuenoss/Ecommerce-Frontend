92c615f02621837996256a4975301c02c95359ff
Joao Bueno <joaobueno121212@gmail.com>
2024-07-06 00:10:03 -0300

    added search funcionality to products screen

--- Ecommerce/src/routes/Products.jsx
+++ Ecommerce/src/routes/Products.jsx
@@ -11,6 +11,8 @@ function Products() {
     const [isLoading, setIsLoading] = useState(true);
     const ProductUrl = '/product';
     const [products, setProducts] = useState([]);
+    const [searchProducts, setSearchProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,6 +21,7 @@ function Products() {
                 setProducts(response.data);
             } catch (err) {
                 setProducts(mockProducts);
+                setFilteredProducts(mockProducts);
             } finally {
                 setIsLoading(false);
             }
@@ -27,12 +30,19 @@ function Products() {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        if(products.length > 0){
+            const lowerCase = searchProducts.toLowerCase();
+            setFilteredProducts(products.filter(p => p.name.toLowerCase().includes(lowerCase)));
+        }
+    }, [searchProducts]);
 
     return (
         <Container className='d-flex flex-wrap justify-content-center mb-4'>
+            <input type='text' onChange={(e) => setSearchProducts(e.target.value)} className='text-search-ctm mt-4 fs-5'></input>
             { isLoading ?
-            <div className='vh-100 d-flex justify-content-center align-items-center'>Loading</div> : 
-            products.map((element) => {
+            <div className='vw-100 vh-100 d-flex justify-content-center align-items-center'>Loading</div> : 
+            filteredProducts.map((element) => {
                 return <CardComponent key={element.id} product={element} />
             })}
         </Container>
