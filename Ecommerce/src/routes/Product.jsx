51a39a502a7760f7f64ffc8e0c17d9c909f8885d
Joao Bueno <joaobueno121212@gmail.com>
2024-07-07 17:56:07 -0300

    making product screen choice photo per category

--- Ecommerce/src/routes/Product.jsx
+++ Ecommerce/src/routes/Product.jsx
@@ -9,6 +9,12 @@ import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
+//products photos
+import elePhoto from '../assets/eletronicsCatepho.jpg';
+import cloPhoto from '../assets/clothingCategory.jpg';
+import booPhoto from '../assets/booksphoto.jpg';
+import comPhoto from '../assets/compAndAcesPho.jpg';
+import hobPhoto from '../assets/HobbiesPho.jpg';
 import photoProduct from '../assets/Generic product.jpg';
 
 function Product() {
@@ -17,6 +23,23 @@ function Product() {
     const productUrl = `/Product/${productId}`
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
+
+    const [imgSrc, setImgSrc] = useState(null);
+
+    useEffect(() => {
+        if (product != null) {
+            if (product.category == 0)
+                setImgSrc(elePhoto)
+            else if (product.category == 1)
+                setImgSrc(cloPhoto)
+            else if (product.category == 2)
+                setImgSrc(booPhoto)
+            else if (product.category == 3)
+                setImgSrc(comPhoto)
+            else if (product.category == 4)
+                setImgSrc(hobPhoto)
+        }
+    }, [product]);
 
     useEffect(() => {
         const handleProduct = async () => {
@@ -50,7 +73,7 @@ function Product() {
                     :
                     <Row className='border me-5 ms-3 ms-md-0 me-md-0'>
                         <Col sm={12} md={5} className='ps-5 py-3'>
-                            <Image src={photoProduct} rounded className='img-product-ctm' />
+                            <Image src={imgSrc} rounded className='img-product-ctm' />
                         </Col>
                         <Col sm={12} md={7} className='ps-5 py-2 d-flex flex-column'>
                             <h2>{product?.name}</h2>
