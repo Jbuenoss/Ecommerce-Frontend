1cce66aba1425f0c2af8eaa94664b0fcd8f48970
Joao Bueno <joaobueno121212@gmail.com>
2024-07-07 17:49:50 -0300

    adding more photos to products

--- Ecommerce/src/components/CardComponent.jsx
+++ Ecommerce/src/components/CardComponent.jsx
@@ -1,15 +1,37 @@
+//component
 import Card from 'react-bootstrap/Card';
-import photoProduct from '../assets/Generic product.jpg';
-
+//products photos
+import elePhoto from '../assets/eletronicsCatepho.jpg';
+import cloPhoto from '../assets/clothingCategory.jpg';
+import booPhoto from '../assets/booksphoto.jpg';
+import comPhoto from '../assets/compAndAcesPho.jpg';
+import hobPhoto from '../assets/HobbiesPho.jpg';
+//css
 import './styleComponent.css';
+//react router and react
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function CardComponent(props) {
+    const [imgSrc, setImgSrc] = useState(null);
+
+    useEffect(() => {
+        if (props.product.category == 0)
+            setImgSrc(elePhoto)
+        else if (props.product.category == 1)
+            setImgSrc(cloPhoto)
+        else if (props.product.category == 2)
+            setImgSrc(booPhoto)
+        else if (props.product.category == 3)
+            setImgSrc(comPhoto)
+        else if (props.product.category == 4)
+            setImgSrc(hobPhoto)
+    }, []);
 
     return (
         <Link to={`/products/${props.product.id}`} className='link-ctm'>
             <Card className='card-ctm mt-3 mx-2'>
-                <Card.Img className='card-img-ctm' variant="top" src={photoProduct} />
+                <Card.Img className='card-img-ctm' variant="top" src={imgSrc} />
                 <Card.Body className='card-body-ctm d-flex flex-column' >
                     <Card.Title className='card-text-ctm'>{props.product.name}</Card.Title>
                     <Card.Text className='mt-auto mb-1 price-text-ctm'>
--- Ecommerce/src/routes/Home.jsx
+++ Ecommerce/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import axios from '../api/axios';
 
 import mockProducts from '../mock/mockProducts';
